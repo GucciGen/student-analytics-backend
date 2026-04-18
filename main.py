25a027e681577b8dce10f8e0155e506d20512306
GucciGen <sviaticrrocer@gmail.com>
2026-04-18 20:51:44 +0300

    Фікс: додав absences у відповідь GET /api/students

--- main.py
+++ main.py
@@ -20,13 +20,20 @@ app = FastAPI(
     description="Бекенд для системи аналітики успішності студентів"
 )
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://student-analytics-backend.onrender.com"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(auth_router)
 
